abbe0aca38b52dd5f330e0e475f67e054b76c9b3
hananhasis <hanan2497@gmail.com>
2022-03-24 16:43:50 +0200

    scss added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
 
+import "./scss/main.scss"
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,23 +1,19 @@
 <template>
   <v-container>
     <app-bar />
-    <!-- <p>{{ $t('message') }}</p> -->
-    <!-- <router-view></router-view> -->
+    <!-- <router-view/> -->
   </v-container>
 </template>
 
 <script>
 import AppBar from "@/components/AppBar.vue";
-// import message from "@/locales/en";
 export default {
   name: "MainPage",
   components: {
     AppBar,
   },
   data() {
-    return {
-      // message,
-    };
+    return {};
   },
 };
 </script>
